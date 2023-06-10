951199083b6c62f619a81cd92c29e947c6806f1b
Tom Kelly <tom-kelly@hotmail.co.uk>
2023-06-10 15:31:32 +0100

    Remove blink

--- led.c
+++ led.c
@@ -1,12 +1,10 @@
 #include "led.h"
 #include <msp430.h>
 
-void led_init(void)
-{
+void led_init(void) {
   P1DIR |= 0x01; // Set P1.0 to output direction
 }
 
-void led_toggle()
-{
-  P1OUT ^= 0x01; // Toggle P1.0 using exclusive-OR
+void led_toggle() {
+  // P1OUT ^= 0x01; // Toggle P1.0 using exclusive-OR
 }
